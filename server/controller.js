e833fb3b52c9ddf1bbc1cd85af4e3fd826631c9b
Yongsoo Lee <yongsoo07@gmail.com>
2019-03-23 13:41:36 -0700

    exercise 4 complete

--- server/controller.js
+++ server/controller.js
@@ -2,16 +2,51 @@ const Todo = require('../database/index.js');
 
 const controller = {
   get: (req, res) => {
-    res.send('hello from get');
+    Todo.find({})
+      .sort({ priority: -1 })
+      .exec((err, docs) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).send(docs);
+        }
+      });
+    // res.send('hello from get');
   },
   post: (req, res) => {
-    res.send('hello from post');
+    const newTodo = req.body;
+    Todo.create(newTodo, (err, docs) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(201).send('added a todo');
+      }
+    });
+    // res.send('hello from post');
   },
   delete: (req, res) => {
-    res.send('hello from delete');
+    const { _id } = req.params;
+    console.log(_id);
+    Todo.deleteOne({ _id }, (err, docs) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(202).send('deleted a todo');
+      }
+    });
+    // res.send('hello from delete');
   },
   put: (req, res) => {
-    res.send('hello from put');
+    const newTodo = req.body;
+    const { _id } = req.params;
+    Todo.findByIdAndUpdate(_id, newTodo, (err, docs) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send('updated a todo');
+      }
+    });
+    // res.send('hello from put');
   },
 };
 
